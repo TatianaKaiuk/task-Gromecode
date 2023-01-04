623e475e1355f637db78e8be9b0f73195c9d59e9
TatianaKaiuk <persik961243@gmail.com>
2023-01-04 13:52:22 +0200

    .

--- js-full/lesson13/task2/calculator.test.js
+++ js-full/lesson13/task2/calculator.test.js
@@ -1,10 +1,10 @@
 import getSum, {
-  getSquaredNumbers,
+  getSquaredArray,
   getOddNumbers,
 } from './calculator.js';
 
 it('should get squared numbers', () => {
-  const result = getSquaredNumbers([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]); 
 });
 
